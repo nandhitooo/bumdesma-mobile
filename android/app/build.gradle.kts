bf9c9447f29ac3f435d86daac982e71472a320bb
Nandhitooo . <dhitopratama2005@gmail.com>
2026-08-13 13:47:58 +0700

    update scan

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.bumdesma.absensi_bumdesma"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
